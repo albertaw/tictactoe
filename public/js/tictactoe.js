04a8072883a0296a75450e26a22ce4bded154177
Alberta Williams <alberta@cerebromusic.com>
2016-02-06 07:20:03 -0600

    refactors init function to allow for one and two player funcionality

--- public/js/tictactoe.js
+++ public/js/tictactoe.js
@@ -16,6 +16,8 @@ var Tictactoe = (function () {
 	var MARK_X = 1;
 	var MARK_O = 2;
 
+	var onePlayer = true;
+
 	//keeps track till we get to end of game
 	var numMoves = 0;
 	
@@ -126,9 +128,13 @@ var Tictactoe = (function () {
 			player.x.state += boardArray[i].state;
 			//switch gamesate
 			gameState = "O_TURN";
-			setTimeout(function(){
+			if (onePlayer && numMoves < 9) {
+				disableBoard();
+				setTimeout(function(){
 					handlePlayer2();
-			}, 1000);
+					enableBoard();
+					}, 500);
+			}
 	
 		} 
 		
@@ -148,20 +154,12 @@ var Tictactoe = (function () {
 	};
 
 	var handlePlayer2 = function () {
-			//$("#message").text(stringForGameState());
 			//mark square
 			AI.update();
 			var i = AI.getMoveNum();
-			$('#'+i).trigger('click');
-			//updateSquare(i);
-			//boardArray[i].mark = MARK_O;
-			//console.log(boardArray[i]);
-			//add points to player x
-			//player.o.state += boardArray[i].state;
-			//switch gamesate
-			//gameState = "X_TURN";
-			//$("#" + i).addClass("oMark");
-			
+			updateSquare(i);
+			$("#" + i).addClass("oMark");
+			$("#message").text(stringForGameState());		
 	};
 
 	var checkForWin = function () {
@@ -216,33 +214,12 @@ var Tictactoe = (function () {
 		return gameStateLabel;
 	};
 	
-	/*
-	var changeState = function(state) {
-		switch (state) {
-			case "intro":
-				handleIntro();
-				break;
-			case "win":
-				Tictactoe.UI.disableBoard();
-				handleWin();
-				break;
-			case "gameOver":
-				handlegameOver();
-				break;
-			case "draw":
-				Tictactoe.UI.disableBoard();
-				handleDraw();
-				break;
-			case "playerX":
-				handlePlayerX();
-				break;
-			case "playerO":
-				setTimeout(function () {handlePlayerO();}, 500);
-				break;
-		}
-	};
-	*/
-	
+	var update = function (self) {
+		var id = self.attr("id").valueOf();
+		updateSquare(id);
+		self.addClass(stringForSquare(id));
+		$("#message").text(stringForGameState());
+	}
 
 	//tasks to rund at the end of each game round
 	var resetGame = function () {
@@ -253,20 +230,13 @@ var Tictactoe = (function () {
 	};
 	
 	//tasks to run when screen first loads	
-	var init = function () {
+	var init = function (players) {
 		setBoardArray();
 		createBoard();
 		$("#message").text(stringForGameState());
 
 		//click listener for board
-		$('.boardCell').click(function () {
-			var id = $(this).attr("id").valueOf();
-			updateSquare(id);
-			//console.log(boardArray[id]);
-			$(this).addClass(stringForSquare(id));
-			$("#message").text(stringForGameState());
-			//handlePlayer2();
-		});
+		enableBoard();
 
 		//click listener for new game
 		$('#btnNewGame').click(function () {
@@ -275,91 +245,26 @@ var Tictactoe = (function () {
 			clearBoard();
 			$("#message").text(stringForGameState());
 		})
-		/*
-		if (players === 1) {
-			handlePlayerO = computer;
-		} else if (players === 2) {
-			handlePlayerO = player2;
-		}
-		*/
+		
+		if (players !== 1) {
+			onePlayer = false;
+		} 
 		console.log("game initialized");
 		
 	};
 	
-	
-
-/*****************************
- GAME CONTROLLER
- ***************************/	
-	
-
-/*
-	 //utility function to display text on screen
-	var setMessage = function (string){
-		$('#message').text(string);
-	};
-	
-
-	 //puts board and game in starting state
-	var enableBtnNewGame = function () {
-		$('#reload').click(function () {
-			clearBoard();
-			cleanup();
-			AIManager.cleanup();
-			initGUI();
-			initGame();
-			AIManager.init();
-		});
-	};
-	
-	//shows the next round button
-	var enableBtnNextRound = function () {
-		$('#btnNextRound').show();
-		//flicker
-		$('#btnNextRound').click(function () {
-			//handle next round
-			clearBoard();
-			cleanup();
-			AIManager.cleanup();
-			setMessage("round " + round);
-			$(this).hide();
-		});
-	};
-	
-	//hides the next round button 
-	var disableBtnNextRound = function () {
-		$('#btnNextRound').hide();
-			
-	};
-	
-	//registers click events on board
-	var enableBoard = function () {
-		$('.boardCell').on('click',function () {
-			Tictactoe.game.update($(this));
-			console.log('board clicked');
-		});
-	};
-	
 	//removes click events from board
 	var disableBoard = function () {
-		var td = document.getElementsByTagName('td');
-		for (var i = 0; i < td.length; i++) {		//for each table cell
-			td[i].onclick = null;
-		}
-		console.log("board disabled");
+		$('.boardCell').off();
 	}
-	
-	//starting state of game elements
-	var initGUI = function () {
-		getBoard();
-		setCells();
-		setMessage("Round " + round);
-		enableBtnNewGame();
-		disableBtnNextRound();
-		$('#x').val(0);
-		$('#o').val(0);
+
+	var enableBoard = function () {
+		$('.boardCell').on('click',function () {
+			update($(this));
+		});
 	};
-	*/
+
+
 return {
 		init: init,
 		cleanup: function () {
@@ -391,9 +296,10 @@ var AI = (function () {
 		//console.log (moves);
 	};
 	var getMoveNum = function () {
+		if (moves) {
+			return moves[0].ID;
+		}
 		
-		console.log("computer move to " + moves[0].ID);
-		return moves[0].ID;
 	}
 
 	return {
