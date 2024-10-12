20f33de45f7abcffb22d3ef2622d0495783bef66
Alberta Williams <alberta04@gmail.com>
2024-10-12 18:37:07 -0500

    add route to server to get the root page

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const http = require('http');
+const path =require('path');
 
 const app = express();
 app.set('port', process.env.PORT || 8080);
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, '/public')));
 
-http.createServer(app).listen(app.get('port'), () => {
-	console.log('App listening on port' + app.get('port'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(app.get('port'), () => {
+	console.log('Server running on port ' + app.get('port'));
 });
